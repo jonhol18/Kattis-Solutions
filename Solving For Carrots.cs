32a05eaba30538f3a7bada6f9ce252d041140dd2
Jonathan Holén <49982913+jonhol18@users.noreply.github.com>
2020-08-23 22:22:19 +0200

    C# Difficulty 1.2

---
+++ Solving For Carrots.cs
@@ -0,0 +1,16 @@
+    class Solving_For_Carrots
+    {
+        public static void Main(string[] args)
+        {
+            string inputTal = Console.ReadLine();
+            string[] charTal = inputTal.Split(' ');
+
+            for (int i = 0; i < int.Parse(charTal[0]); i++)
+            {
+                string ThrowAway = Console.ReadLine();
+            }
+
+            Console.WriteLine(charTal[1]);
+        }
+    }
+}
